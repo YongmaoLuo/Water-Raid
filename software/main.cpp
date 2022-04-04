250eee3b0b54ac546916969977b1d3a9fd1f86dc
YongmaoLuo <576146819@qq.com>
2022-04-04 17:01:39 -0400

    Add main.cpp

---
+++ software/main.cpp
@@ -0,0 +1,16 @@
+//
+// Created by Yongmao Luo on 4/1/22.
+//
+
+#include "bullet.h"
+#include "game_senario.h"
+#include "battleship.h"
+#include "airplane.h"
+#include "enemy_plane.h"
+#include "fuel_tank .h"
+
+#define MAX_SPRITES_NUM 8
+#define MAX_BULLET_NUM 5
+
+Bullet bullets[MAX_BULLET_NUM];
+
