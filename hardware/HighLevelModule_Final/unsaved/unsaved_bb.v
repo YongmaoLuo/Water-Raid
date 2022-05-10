6cd73f0a23942abea03d044551cb01647a25f468
tls2160 <tls2160@micro23.ee.columbia.edu>
2022-05-10 16:05:09 -0400

    Fixed audio

---
+++ hardware/HighLevelModule_Final/unsaved/unsaved_bb.v
@@ -0,0 +1,12 @@
+
+module unsaved (
+	vga_ball_0_clock_clk,
+	audio_0_clk_clk,
+	vga_ball_0_reset_reset,
+	audio_0_reset_reset);	
+
+	input		vga_ball_0_clock_clk;
+	input		audio_0_clk_clk;
+	input		vga_ball_0_reset_reset;
+	input		audio_0_reset_reset;
+endmodule
---
+++ hardware/HighLevelModule_Final/unsaved/unsaved_inst.v
@@ -0,0 +1,7 @@
+	unsaved u0 (
+		.vga_ball_0_clock_clk   (<connected-to-vga_ball_0_clock_clk>),   // vga_ball_0_clock.clk
+		.audio_0_clk_clk        (<connected-to-audio_0_clk_clk>),        //      audio_0_clk.clk
+		.vga_ball_0_reset_reset (<connected-to-vga_ball_0_reset_reset>), // vga_ball_0_reset.reset
+		.audio_0_reset_reset    (<connected-to-audio_0_reset_reset>)     //    audio_0_reset.reset
+	);
+
